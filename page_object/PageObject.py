764e2b4e5f318f6465355290cce8062358905d49
tmm6 <tmm6@cesar.school>
2023-05-26 23:49:28 -0300

    Search e Delete Employee

--- page_object/PageObject.py
+++ page_object/PageObject.py
@@ -3,11 +3,21 @@ from selenium.webdriver.common.by import By
 
 
 class PageObject:
+    #Locators
+    ## Elements
+    css_type_add_button = 'oxd-button--secondary'
+    css_class_option_popup_delete = 'orangehrm-button-margin'
+
+    ## Texts
+    text_confirm_popup_delete = 'Yes, Delete'
+
     def __init__(self, driver=None):
         if driver:
             self.driver = driver
         else:
             self.driver = webdriver.Chrome()
+            # Ajustar tamanho da tela
+            self.driver.set_window_size(1200, 920)
             self.driver.implicitly_wait(3)
 
     def verify_url(self, correct_url):
@@ -17,3 +27,30 @@ class PageObject:
         current_title = self.driver.find_element(By.CLASS_NAME, element_title).text
         return correct_title == current_title
 
+    def click_correct_button(self, element=css_type_add_button, expect_title=""):
+        """
+        Este método irá clicar no botão de Search ou de +Add. A escolha é de acordo com o parâmetro "expect_title"
+        O método consiste em realizar uma busca por todos os elementos que correspondem ao botão.
+        Em seguida, compara cada elemento da lista de elementos encontrados com o botão Search ou Add em que a
+        escolha é passada por parâmetro.
+        Por fim, clica no botão.
+        """
+        buttons_list = self.driver.find_elements(By.CLASS_NAME, element)
+        for i in range(len(buttons_list)):
+            current_button = buttons_list[i]
+            if current_button.text == expect_title:
+                current_button.click()
+
+    def confirm_popup_delete_data(self, option=text_confirm_popup_delete):
+        """
+        Este método pode ser utilizado quando se tem popups de confirmação de exclusão de algum dado.
+        :param option: Representa a escolha de qual opção no popup é selecionada. Pode ser Cancelar ou Confirmar.
+        :return: O dado é deletado ou não de acordo com o parâmetro
+        """
+        options_popup_delete = self.driver.find_elements(By.CLASS_NAME, self.css_class_option_popup_delete)
+        for i in range(len(options_popup_delete)):
+            current_option = options_popup_delete[i]
+            print(current_option.text)
+            if current_option == option:
+                print('Deu certo')
+                #current_option.click()
--- page_object/employee/EmployeePageObject.py
+++ page_object/employee/EmployeePageObject.py
@@ -7,9 +7,10 @@ from page_object.PageObject import PageObject
 
 class EmployeePageObject(PageObject):
     # Locators
+    ## URLs
     url_add_employee = 'https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee'
-    css_type_add_button = 'oxd-button--secondary'
-    add_employee_title = 'Add'
+
+    ## Elements
     css_name_first_name = 'firstName'
     css_name_middle_name = 'middleName'
     css_name_last_name = 'lastName'
@@ -17,23 +18,38 @@ class EmployeePageObject(PageObject):
     css_class_toast = 'oxd-toast-container--toast'
     css_class_title_add_employee = 'orangehrm-main-title'
     css_class_name_employee = 'orangehrm-edit-employee-name'
+    css_placeholder_employee_name = '[placeholder="Type for hints..."]'
+    css_class_cells_employees = '[role = "cell"]'
+    css_class_delete_employee_button = 'bi-trash'
 
-    title_add_employee = 'Add Employee'
-    first_name = 'Barbie'
-    middle_name = 'Millicent'
-    last_name = 'Roberts'
-
+    ## Text
+    text_add_employee_button = 'Add'
+    text_search_employee = 'Search'
+    text_title_add_employee = 'Add Employee'
+    text_first_name = 'Barbie'
+    text_middle_name = 'Millicent'
+    text_last_name = 'Roberts'
 
     def __init__(self, driver):
         super().__init__(driver=driver)
 
+    # Métodos para preencher os campos
+    def first_name_field(self):
+        self.driver.find_element(By.NAME, self.css_name_first_name).send_keys(self.text_first_name)
+
+    def middle_name_field(self):
+        self.driver.find_element(By.NAME, self.css_name_middle_name).send_keys(self.text_middle_name)
+
+    def last_name_field(self):
+        self.driver.find_element(By.NAME, self.css_name_last_name).send_keys(self.text_last_name)
+
+    def click_button_save_employee(self):
+        self.driver.find_element(By.CSS_SELECTOR, self.css_button_save).click()
+
+    # Método para acessar a tela de adicionar um funcionário.
     def access_add_employee_page(self):
-        buttons_list = self.driver.find_elements(By.CLASS_NAME, self.css_type_add_button)
-        for i in range(len(buttons_list)):
-            current_button = buttons_list[i]
-            if current_button.text == self.add_employee_title:
-                current_button.click()
-        return self.verify_title(self.title_add_employee, self.css_class_title_add_employee) and \
+        self.click_correct_button(expect_title=self.text_add_employee_button)
+        return self.verify_title(self.text_title_add_employee, self.css_class_title_add_employee) and \
             self.verify_url(self.url_add_employee)
 
     def add_employee(self):
@@ -42,30 +58,50 @@ class EmployeePageObject(PageObject):
         self.last_name_field()
         self.click_button_save_employee()
         # Verificar se o toast de confirmação do cadastro é exibido
-        # try:
-        #     time.sleep(2)
-        #     self.driver.find_element(By.CLASS_NAME, self.css_class_toast)
-        #     # Melhorar a verficação do toast
-        # except:
-        #     return False
-
-    def first_name_field(self):
-        self.driver.find_element(By.NAME, self.css_name_first_name).send_keys(self.first_name)
-
-    def middle_name_field(self):
-        self.driver.find_element(By.NAME, self.css_name_middle_name).send_keys(self.middle_name)
-
-    def last_name_field(self):
-        self.driver.find_element(By.NAME, self.css_name_last_name).send_keys(self.last_name)
-
-    def click_button_save_employee(self):
-        self.driver.find_element(By.CSS_SELECTOR, self.css_button_save).click()
+        try:
+            time.sleep(2)
+            self.driver.find_element(By.CLASS_NAME, self.css_class_toast)
+            # Melhorar a verficação do toast
+        except:
+            return False
 
     def verify_employee_info(self):
+        self.driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/38')
         current_name = self.driver.find_element(By.CLASS_NAME, self.css_class_name_employee).text
-        print(current_name)
-        correct_name = ' '.join([self.first_name, self.last_name])
+        #print(current_name.text)
+        correct_name = ' '.join(["Thays", "Melo"])
+        #correct_name = ' '.join([self.text_first_name, self.text_last_name])
         print(correct_name)
-        return correct_name == current_name
+        print(correct_name == current_name)
+
+    def search_employee(self):
+        list_elements_search = self.driver.find_elements(By.CSS_SELECTOR, self.css_placeholder_employee_name)
+        employee_name = list_elements_search[0]
+        employee_name.send_keys('Charlie')
+        self.click_correct_button(expect_title=self.text_search_employee)
+        # Trocar por um WebDriverWait
+        time.sleep(1)
+        list_employees = self.driver.find_elements(By.CSS_SELECTOR, self.css_class_cells_employees)
+        if list_employees[2].text == 'Charlie':
+            return True
+
+    def delete_employee(self):
+        """
+        1 - Pesquisar um funcionario
+        2 - Verificar se ele está na lista
+        3 - Clicar no ícone da lixeira
+        4 - Clicar em confirmar no popup
+        5 - Verificar toast
+        6 - Verificar que tem 0 ocorrências na lista de funcionários
+        """
+        if self.search_employee():
+            time.sleep(2)
+            self.driver.find_element(By.CLASS_NAME, self.css_class_delete_employee_button).click()
+            # Trocar por um WebDriverWait
+            time.sleep(1)
+            self.confirm_popup_delete_data()
 
 
+
+
+        
